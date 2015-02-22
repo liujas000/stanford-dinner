e68d242093f474524fd2a4a9844a74bdb5eaddfa
Jason Liu <liujas000@gmail.com>
2015-02-22 03:51:18 -0800

    working code

--- stanford-dine/app/assets/javascripts/welcome.js
+++ stanford-dine/app/assets/javascripts/welcome.js
@@ -1,51 +1,26 @@
-
-
-$(document).ready(function() {
-  $("[id^='score']").each(function(index) {
-  var n = parseInt($(this).getText());
-    if(n > 0) {
-      $(this).css('color', 'green');
-    }
-    else if(n < 0) {
-      $(this).css('color', 'red');
-    }
-    else {
-      $(this).css('color', 'black');
-    }
-  });
-
-  $('.plus-button').click(function(){
+$(document).ready(function(){
+  
+	$('.plus-button').click(function(){
+    
     var id = $(this).parent().attr("id");
     var button = $(this).parent;
     var change = 1;
-
-    if($(this).hasClass('active')) {
-        $(this).removeClass('active');
-        change *= -1;
-    }
-    else {
-        $(this).addClass('active');
-        if($(this).parent().find('.minus-button').hasClass('active')) {
-            $(this).parent().find('.minus-button').removeClass('active');
-            change *= 2;
-        }
-    }
-
-    var newScore = parseInt($("#score"+id).text()) + change;
-    $("#score"+id).text(newScore);
-    if(k > 0) {
-      $(newScore).css('color', 'green');
-    }
-    else if(k < 0) {
-      $(newScore).css('color', 'red');
-    }
-    else {
-      $(newScore).css('color', 'black');
-    }
-
   
-    $.ajax({
-      type: "POST",
+    if($(this).hasClass('active')) {
+       	$(this).removeClass('active');
+      	change *= -1;
+    }
+    else {
+    	$(this).addClass('active');
+      	if($(this).parent().find('.minus-button').hasClass('active')) {
+            $(this).parent().find('.minus-button').removeClass('active');
+        	change *= 2;
+      	}
+    }
+    $("#score"+id).text(parseInt($("#score"+id).text()) + change);
+  
+		$.ajax({
+		  type: "POST",
       url:"/vote",
       data: {
         hall_id: id,
@@ -58,36 +33,31 @@ $(document).ready(function() {
           console.log(data);
           console.log("ajax failure");
       }
-    })
-  });
-  $('.minus-button').click(function(){
+		})
+    
+	});
+});
+
+$(document).ready(function(){
+	$('.minus-button').click(function(){
     var id = $(this).parent().attr("id");
     var change = -1;
     
     if($(this).hasClass('active')) {
-        $(this).removeClass('active');
-        change *= -1;
+       	$(this).removeClass('active');
+      	change *= -1;
     }
     else {
-      $(this).addClass('active');
-        if($(this).parent().find('.plus-button').hasClass('active')) {
-          $(this).parent().find('.plus-button').removeClass('active');
-          change *= 2;
-        }
+    	$(this).addClass('active');
+      	if($(this).parent().find('.plus-button').hasClass('active')) {
+       		$(this).parent().find('.plus-button').removeClass('active');
+        	change *= 2;
+      	}
     }
-    var newScore = parseInt($("#score"+id).text()) + change;
-    $("#score"+id).text(newScore);
-    if(k > 0) {
-      $(newScore).css('color', 'green');
-    }
-    else if(k < 0) {
-      $(newScore).css('color', 'red');
-    }
-    else {
-      $(newScore).css('color', 'black');
-    }
-    $.ajax({
-      type: "POST",
+    $("#score"+id).text(parseInt($("#score"+id).text()) + change);
+      
+		$.ajax({
+		  type: "POST",
       url:"/vote",
       data: {
         hall_id: id,
@@ -100,11 +70,6 @@ $(document).ready(function() {
           console.log(data);
           console.log("ajax failure");
       }
-    })
-  });
+		})
+	});
 });
-
-
-
-
-
