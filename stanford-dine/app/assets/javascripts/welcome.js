a3ccc6a5b98d63ca2e10f862532cfe3f684ad9e1
Jason Liu <liujas000@gmail.com>
2015-02-21 21:40:43 -0800

    better jquery

--- stanford-dine/app/assets/javascripts/welcome.js
+++ stanford-dine/app/assets/javascripts/welcome.js
@@ -1,11 +1,40 @@
-
-$(function(){
-    $('#plus-button').on('click', function(e){
-        alert("BUTTON WAS PRESSED");
-        console.log("button pressed");
-        $.ajax({
-          type:'POST',
-          url:'/vote/'+ $(this).attr('id') + '/1';
-        })
-    });
+//
+// $(function(){
+//     $('#plus-button').on('click', function(e){
+//         alert("BUTTON WAS PRESSED"å);
+//         console.log("button pressed");
+//         $.ajax({
+//           type:'POST',
+//           url:'/vote/'+ $(this).attr('id') + '/1';
+//         })
+//     });
+// });
+$(document).ready(function(){
+	$('.plus-button').click(function(){
+    var abc = $(this).parent().attr("id");
+		alert("plus button pressed " + abc);
+		$.ajax({
+		  type: "POST",
+      url:"/vote",
+      data: {
+        hall_id: abc,
+        vote: 1
+      },
+      success: function() {
+        alert("ajax success");
+      },
+      error: function() {
+        alert("ajax failure");
+      }
+		})
+    
+	});
 });
+
+$(document).ready(function(){
+	$('.minus-button').click(function(){
+    var abc = $(this).parent().attr("id");
+		alert("minus button pressed" + abc);
+	});
+});
+alert("javascript started");
