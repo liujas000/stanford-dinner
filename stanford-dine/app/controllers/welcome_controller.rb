ddbc815e1d419930d38771b3a9fbabeee430b723
Jason Liu <liujas000@gmail.com>
2015-02-22 01:47:04 -0800

    yost murray east

--- stanford-dine/app/controllers/welcome_controller.rb
+++ stanford-dine/app/controllers/welcome_controller.rb
@@ -1,6 +1,23 @@
 class WelcomeController < ApplicationController
   def index
     @halls = Hall.all
+    @weekday = Time.now.strftime('%A')
+    hour = Time.now.strftime("%K").to_i
+    if(@weekday == "Saturday" || @weekday == "Sunday")
+      if(hour > 7 && hour < 16) 
+        @meal = "Brunch"
+      else
+        @meal = "Dinner"
+      end
+    else
+      if(hour > 7 && hour < 11)
+        @meal = "Breakfast"
+      elsif(hour < 16)
+        @meal = "Lunch"
+      else
+        @meal = "Dinner"
+      end
+    end
   end
 end
 
