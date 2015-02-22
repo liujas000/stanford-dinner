e95354d9e487a89260b9f9dbdaa2389703056416
Max Spero <mmspero@gmail.com>
2015-02-21 21:10:49 -0800

    ajax stuff

--- stanford-dine/app/controllers/welcome_controller.rb
+++ stanford-dine/app/controllers/welcome_controller.rb
@@ -4,7 +4,8 @@ class WelcomeController < ApplicationController
   end
 
   def vote
-    @hall = Article.find(params[:id])
-    vote_val = params[:val]; 
+    @hall = Article.find(params[:hall_id])
+    vote_val = params[:vote]; 
+    puts(vote_val);
   end
 end
